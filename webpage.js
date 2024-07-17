a415b7b0ee52a2c0463959257de74801f267b341
Travi-s <tbmail314@gmail.com>
2024-07-17 12:30:36 -0700

    Update webpage.js
    button code added to webpage.js

--- webpage.js
+++ webpage.js
@@ -43,3 +43,9 @@ Plotly.newPlot(
   ],
   layout
 );
+const button = document.querySelector('#linkBtn');
+
+button.addEventListener('click', function() {
+  window.open('https://youtu.be/dQw4w9WgXcQ', '_blank');
+  alert('I lied.')
+})
